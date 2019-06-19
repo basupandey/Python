71658dab108c13fa80c2f59f7b24af99032d2a83
Basu Pandey <basupandey01@gmail.com>
2019-06-19 12:29:55 +0545

    introduction to python including if elif,input and converting input into string

---
+++ basu.py
@@ -0,0 +1,74 @@
+# python introduction
+# print('Hello python')
+
+# print('mother\'s')
+
+# print("Mother's")
+# x=10
+#
+# print(x)
+#
+# name='admin'
+# age=20
+#
+# print("Your name is {} and your age is {}".format(name, age))
+
+# python is in bulid library we are just using it python is case sensitive
+
+# name='Ram'
+
+# print(type(name))
+
+# name=1234
+
+# print(type(name))
+
+# interger has 3 types like int, float, complex
+
+# name=1234.45
+
+# print(type(name))
+
+# name=1234j
+
+# print(type(name))
+
+# array is to reduce memory loaction
+
+# list index starts from 0
+
+# name=['ram','234434','hari']
+# print(type(name))
+# name={1,3,3}
+# print(name)
+# set has no duplicate value
+
+# x=20
+# y=100
+# if x>y:
+#     print('x')
+# else:
+#     print('y')
+
+# if only boolean value representation
+# python only make line identation no need curly barcket
+# x=input('enter firsr number')
+# y=input('enter second number')
+# if x>y:
+#     print('x')
+# else:
+#     print('y')
+# multiple variable in the same line
+
+print('enter the first number')
+x = int(input())
+print('enter the second number')
+y = int(input())
+print('enter the third number')
+z = int(input())
+if x > y and x > z:
+    print('x is the greater number')
+elif y > x and y > z:
+    print('y')
+else:
+    print('z')
