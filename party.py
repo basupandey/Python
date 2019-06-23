857274346ab7e23503b2dc6dbff415d3316097b2
Basu Pandey <basupandey01@gmail.com>
2019-06-23 17:29:12 +0545

    Nested if loop to determine the age group who can go for party

---
+++ party.py
@@ -0,0 +1,13 @@
+age = int(input("Enter your age"))
+if age < 16:
+    if age > 16<40:
+        g = "you can go to party"
+    else:
+        g="Sorry you are young and not allowed to go for party"
+else:
+    if age>40:
+        g="Sorry you are too old for party"
+    else:
+        g="you can go to party"
+
+print(g)
